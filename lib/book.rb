54c26f10dc39486cff4bce98aea8622f430d9b11
yeohoon yoon <yhoon.yoon@gmail.com>
2022-02-23 11:05:02 -0500

    completed

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,17 @@
+class Book
+  attr_accessor :title, :author, :page, :genre
+  def initialize (title, author, page, genre)
+    @title = title
+    @author = author
+    @page = page
+    @genre = genre
+  end
 
+  def book_info
+    "title: #{title} author: #{author} page: #{page} genre: #{genre}" 
+  end
+end
 
+book1 = Book.new("Sam's story", "Sam", 1000, "fiction")
+puts book1.book_info
+puts book1.title
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,43 @@
-# Make your shoe class here!
+# Make your shoe classcla here!
+class Shoe
+  attr_accessor :brand, :color, :size, :material, :condition
+    def initialize(brand, color, size, material, condition)
+      @brand = brand
+      @color = color
+      @size = size
+      @material = material
+      @condition = condition
+    end
+
+      def new_shoe 
+        "#{brand}#{color}#{size}#{material}#{condition}"
+      end
+      
+      def check_condition
+        if @condition == "new"
+          return "#{brand} makes the shoe's condition #{condition}"
+        else
+          return "shoes has been #{condition}"
+      end
+    end
+end
+
+shoe1 = Shoe.new("nike","red", 130,"leather", "new")
+shoe2 = Shoe.new("adidas","whie", 230,"vinyl", "repaired")
+
+
+
+shoes_box = []
+def shoes_list (shoes_box, item)
+  shoes_box << item
+  message = "This is what we have :"
+  if shoes_box.length == 0
+    puts "list is empty"
+  end
+  shoes_box.each_with_index do |value, index|
+  message += "#{index.to_i+1}. #{value}"
+  end
+  return "#{message}"
+end  
+
+puts shoes_list(shoes_box, "Nike")
